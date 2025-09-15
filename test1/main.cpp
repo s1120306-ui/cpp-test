dc28608b989015b515cdd0fe58c609ecaa14fb32
s1120306-ui <s1120306@dmhs.kh.edu.tw>
2025-09-15 03:36:22 +0000

    你媽交了

---
+++ test1/main.cpp
@@ -0,0 +1,7 @@
+ #include <iostream>
+ int main() {
+   int x;            // 宣告（declaration）：跟編譯器說「我需要一個 int 變數叫 x」
+   x = 5;            // 指派（assignment）：把數值 5 放到 x 裡
+   std::cout << x;   // 這時輸出會是 5
+return 0;
+}
