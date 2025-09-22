cc8230155b0e8c485aec410a28ead2da7442916f
s1120306-ui <s1120306@dmhs.kh.edu.tw>
2025-09-22 03:33:44 +0000

    作業2

--- test1/main.cpp
+++ test1/main.cpp
@@ -1,7 +1,15 @@
- #include <iostream>
- int main() {
-   int x;            // 宣告（declaration）：跟編譯器說「我需要一個 int 變數叫 x」
-   x = 5;            // 指派（assignment）：把數值 5 放到 x 裡
-   std::cout << x;   // 這時輸出會是 5
-return 0;
+#include <iostream>
+
+int main() {
+    double celsius;
+    double fahrenheit;
+
+    std::cout << "請輸入華氏溫度!\n";
+    std::cin >> fahrenheit;
+
+    celsius = (fahrenheit - 32) / 9 * 5;
+    
+    std::cout << "攝氏溫度為: " << celsius << " 度\n";
+    return 0;
 }
+
